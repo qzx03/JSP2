fb1a0a7c88b0fe70c9e6206729b16a595dad4ba0
ahmed <ahmedelmorabiti123460@gmail.com>
2025-09-17 11:46:11 +0100

    main

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let nombre1 = 10;
 let nombre2 = 5;
 let somme = nombre1 + nombre2;
 console.log("La somme est : " + somme);
+
+
+let age = 20;
+if (age < 18) {
+    console.log("You are a minor");
+} else if (age === 18) {
+    console.log("You have just reached adulthood");
+} else {
+    console.log("You are an adult");
+}
